13d74eb8afddc927ef7234a3095cb43726169755
Matheus Batista <dev.batista.matheus@gmail.com>
2025-07-04 09:37:21 -0300

    adicionando o exercicio 3

---
+++ exercicio_3.py
@@ -0,0 +1,7 @@
+
+
+data_do_usuario = input("Insira uma data no formato dd/mm/aaaa: ")
+data_separada = data_do_usuario.split("/")
+print(f"O elemento 1 e o:{data_separada[0]}")
+print(f"O elemento 2 e o:{data_separada[1]}")
+print(f"O elemento 3 e o:{data_separada[2]}")
