bf795973f80f1b58ca9d337ab41fc4ad05cba113
Matheus Batista <dev.batista.matheus@gmail.com>
2025-07-04 08:47:30 -0300

    exercicio_1.py

---
+++ exercicio_1.py
@@ -0,0 +1,27 @@
+print(6 % 4)
+
+nome_aluno = "Matheus@gmail.com"
+
+print(type("Matheus"))
+
+print(type("nome_aluno"))
+
+print(nome_aluno.split("@"))
+
+valor_1 = True
+valor_2 = False
+valor_3 = True
+Valor_4 = False
+
+print(valor_1 and valor_3)
+print(valor_1 or valor_3)
+print(valor_1== valor_3)
+print(valor_1 != valor_3)
+
+
+numero_01 = int(input("insira um número inteiro: "))
+numero_02 = int(input("insira outro número inteiro: "))
+
+resultado = numero_01 // numero_02
+
+print(resultado)
