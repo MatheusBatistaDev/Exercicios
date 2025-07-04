528f5fbeeaba158ceef7fd427b4daafe1441dec2
Matheus Batista <dev.batista.matheus@gmail.com>
2025-07-04 10:25:25 -0300

    adicionando teste 3

---
+++ testes_3.py
@@ -0,0 +1,15 @@
+idade = 30
+
+IDADE_MINIMA_PARA_DIRIGIR = 18
+IDADE_PARA_TIRAR_CARTEIRA = 18
+
+if idade < IDADE_MINIMA_PARA_DIRIGIR:
+    print("não pode dirigir")
+
+elif idade == IDADE_PARA_TIRAR_CARTEIRA:
+    PRINT("Pode tirar carteira de motorista")
+
+else:
+    print("Pode dirigir")
+    
+
